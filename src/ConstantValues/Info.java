22d7adaa098263decb77a82fb08ee3b894429f94
minttea25 <minttea.dev@gmail.com>
2022-09-18 22:47:40 +0900

    modified setting team number 2
    added prefix about Sections in Settings.json

--- src/ConstantValues/Info.java
+++ src/ConstantValues/Info.java
@@ -1,8 +1,8 @@
 package ConstantValues;
 
 public class Info {
-    public static String VERSION = "2.0.0";
-    public static String LAST_UPDATE = "220917";
+    public static String VERSION = "2.1.0";
+    public static String LAST_UPDATE = "220918";
     public static String DEV = "minttea25";
     public static String DEV_URL = "https://github.com/minttea25";
     public static String REPOSITORY = "https://";
